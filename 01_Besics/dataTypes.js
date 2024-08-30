35c938017645c1fdfc546e9b251ef70c00da1e53
prd-tarkeshwar <151716288+prd-tarkeshwar@users.noreply.github.com>
2024-08-30 18:19:17 +0000

    dataTypes

---
+++ 01_Besics/dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"//treat all code JS as newer version
+//alert(3+3) // we are using nodejs, not browser
+
+console.log(3
+    +3)//code readability should be high
+console.log("Tarkeshwar")
+
+let name="tarkeshwar"
+let age=20
+let isLoggedId=false
+
+//number=> 2 to power 53
+//bigint
+//string=>""
+//boolean=>true/false
+//undefined=>
+//symbol=>uniqe
+
+//object
+console.log(typeof "tarkeshwar");
+console.log(typeof age);
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
