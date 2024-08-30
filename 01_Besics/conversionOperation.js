7b9aa94d3176bd97797c3dd5989e9d416c6c79b3
prd-tarkeshwar <151716288+prd-tarkeshwar@users.noreply.github.com>
2024-08-30 19:00:43 +0000

    conversion of dataTypes

---
+++ 01_Besics/conversionOperation.js
@@ -0,0 +1,26 @@
+let score=true
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber=Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+//"33"=>33
+//"33sdfd"=>NaN
+//true => 1; false=>0
+
+let isLoggeIn=0
+let booleanIsLoggeIn=Boolean(isLoggeIn)
+//console.log(booleanIsLoggeIn);
+
+//1=>true; 0=>false
+//""=>false
+//"tarkeshwar"=>true
+
+let someNumber = 33
+let stringNumber=String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
