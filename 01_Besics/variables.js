f168c28afc2c51514f15bfe9d050815039c591b1
prd-tarkeshwar <151716288+prd-tarkeshwar@users.noreply.github.com>
2024-08-30 18:19:00 +0000

    variables

---
+++ 01_Besics/variables.js
@@ -0,0 +1,23 @@
+const accountId = 144553
+let accountEmail = "tarkeshwarparit0101@gmail.com"
+var accountPassword="12345"
+accountCity="Jaipur"
+let accountState;
+
+//accountId=2
+accountEmail="fhgftyry@com"
+accountPassword="565547"
+accountCity="uiyutyry"
+
+console.log(accountId);
+//console.log(accountEmail);
+//console.log(accountPassword);
+//console.log(accountCity);
+
+/* Prefer not to use var
+because of issue in block scope and functional scope
+*/
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+
+
