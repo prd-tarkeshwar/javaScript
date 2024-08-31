4bc601434b7a38d310435b33013fa046dd5efa28
prd-tarkeshwar <151716288+prd-tarkeshwar@users.noreply.github.com>
2024-08-31 21:19:34 +0000

    1DetaTypes-Summary

--- 01_Besics/DetaTypes-Summary.js
+++ 01_Besics/DetaTypes-Summary.js
@@ -10,7 +10,7 @@ const outisdeTemp = null // data types : object
 let userEmail; //  data types : undefined
 const id = Symbol('123') // data types : Symbol
 const anotherId = Symbol('123') // data types : Symbol
-console.log(id === anotherId);
+//console.log(id === anotherId);
 
 const bigNumber = 3896564587986656873134n //bigint
 
@@ -28,4 +28,26 @@ const myFunction = function(){ //data types : function
     console.log("Hello World");
 }
 
-console.log(typeof myFunction);
+//console.log(typeof myFunction);
+
+
+//******************************Memory************************************ */
+//Stack(Primitive), Heap(Non-Primitive)
+
+let myYoutubename = "funny videos"
+let anothername = myYoutubename
+anothername="kapilfunnyvideos"
+console.log(myYoutubename);
+console.log(anothername);
+
+let userOne = {
+    email: "tark@gmail.com",
+    upi : "user@bl"
+}
+
+let userTwo = userOne
+userTwo.email = "tarkeshwar@gmail.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
