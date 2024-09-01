0407e5714145b6019a977e3ab7fce1b31700a8c9
prd-tarkeshwar <151716288+prd-tarkeshwar@users.noreply.github.com>
2024-09-01 19:02:42 +0000

    Datesin

---
+++ 01_Besics/Datesin.js
@@ -0,0 +1,23 @@
+//Date
+
+// let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleDateString());
+// console.log(typeof myDate);
+
+// let myCreateDate = new Date(2023, 0, 23)
+// let myCreateDate = new Date(2023, 0, 23, 5, 3)
+let myCreateDate = new Date("2024=02-14")
+// console.log(myCreateDate.toLocaleDateString());
+
+// let myTimeStap = Date.now()
+// console.log(myTimeStap);
+// console.log(myCreateDate.getTime());
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth());
+console.log(newDate.getDay());
+
+
