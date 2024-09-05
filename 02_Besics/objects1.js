106f8b74e67c9be5ca264a18377740f7077c4d0a
prd-tarkeshwar <151716288+prd-tarkeshwar@users.noreply.github.com>
2024-09-05 18:38:04 +0000

    top object

---
+++ 02_Besics/objects1.js
@@ -0,0 +1,53 @@
+// const tinderUser = new Object()
+const tinderUser ={}
+tinderUser.id="123abc"
+tinderUser.name="Tarkeshwar"
+tinderUser.isLoggedIn=false
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email: "tarkeshwar01@gmal.com",
+    fullnamme: {
+        userfullname: {
+            firstname: "Tarkeshwar",
+            lastname: "Parit"
+        }
+    }
+}
+// console.log(regularUser.fullnamme.userfullname.lastname);
+
+const obj1 = {1:"a", 2:"b"}
+const obj2 = {3:"a", 4:"b"}
+const obj4 = {5:"a", 6:"b"}
+
+// const obj3 = {obj1,obj2}
+// const obj3 = Object.assign({},obj1,obj2,obj4)
+// console.log(obj3);
+const obj3={...obj1,...obj2}
+// console.log(obj3);
+
+const users = [
+    {
+        id:1,
+        email: "t@mail,con"
+    },
+    {
+        id:1,
+        email: "t@mail,con"
+    },
+    {
+        id:1,
+        email: "t@mail,con"
+    },
+    {
+        id:1,
+        email: "t@mail,con"
+    }
+]
+users[1].email
+console.log(tinderUser);
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
